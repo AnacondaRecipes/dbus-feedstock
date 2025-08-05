8475b0664f45c62ced2a812f73bd3c03b64c1ee1
Mikolaj Gralczyk <mgralczyk@anaconda.com>
2025-08-05 17:05:49 +0200

    Whitelist missing dso

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,18 +15,3 @@ if [[ "${target_platform}" != osx-* ]]; then
    meson test -C build --print-errorlogs
 fi
 meson install -C build
-
-# Ensure the soname symlink exists
-if [[ -f "${PREFIX}/lib/libdbus-1.so.3.38.3" ]]; then
-    cd "${PREFIX}/lib"
-    ln -sf libdbus-1.so.3.38.3 libdbus-1.so.3
-    echo "Created symlink: libdbus-1.so.3 -> libdbus-1.so.3.38.3"
-elif [[ -f "${PREFIX}/lib64/libdbus-1.so.3.38.3" ]]; then
-    cd "${PREFIX}/lib64"
-    ln -sf libdbus-1.so.3.38.3 libdbus-1.so.3
-    echo "Created symlink in lib64: libdbus-1.so.3 -> libdbus-1.so.3.38.3"
-fi
-
-# Verify library installation
-find "${PREFIX}" -name "*libdbus*" -type f | sort
-find "${PREFIX}" -name "*libdbus*" -type l | sort
