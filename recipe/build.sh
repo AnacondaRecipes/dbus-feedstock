4e661099af5f9f6c564f8ac3f3ba9c511fa90805
Mikolaj Gralczyk <mgralczyk@anaconda.com>
2025-08-22 21:05:22 +0200

    Clarify skipping tests on macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,6 +13,11 @@ meson setup build \
   -Dlaunchd_agent_dir="${PREFIX}"
 
 meson compile -C build
+
+# Skip tests on macOS as 2 of them fail because of the expected behavior:
+# - X11 autolaunch disabled
+# - known issue with bus socket creation
+# No easy way to skip single tests on meson.
 if [[ "${target_platform}" != osx-* ]]; then
    meson test -C build --print-errorlogs
 fi
